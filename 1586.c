7e4609236776b9406615e6507680d8b353d7a2a9
Flávio <flavio@localhost.localdomain>
2017-03-07 15:43:20 -0300

    1586

--- 1586.c
+++ 1586.c
@@ -8,20 +8,98 @@
 
 int main(void){
   int t;
-  int qnt;
+  int qnt = 1;
+  float peso = 0;
   char palavra[SIZE];
-  float mol;
+  float mol = 0;
   int i,j;
+  char numero[2];
+  int sinal = 0;
+  int proximaNum = 0;
 
   scanf("%d", &t);
   for(i = 0; i < t; i++){
     scanf("%s", palavra);
     for(j = 0; j < strlen(palavra); j++){
-      if (palavra[j] >= 48 && palavra[j] <= 57){
-        score = score + 1 + acc;
-        acc = acc + 1;
+      proximaNum = 0;
+      switch(palavra[j]){
+        case 'O':
+          peso = OXI;
+          break;
+        case 'C':
+          peso = CARB;
+          break;
+        case 'H':
+          peso = HID;
+          break;
+        case 'N':
+          peso = NIT;
+          break;
+      }
+      if((j + 1 < strlen(palavra)) && (palavra[j + 1] >= 48) && (palavra[j + 1] <= 57)){
+        proximaNum = 1;
+      }
+      if(!proximaNum){
+        mol = mol + peso;
       } else {
-        acc = 0;
+        j++;
+        if((j + 1 < strlen(palavra)) && (palavra[j + 1] >= 48) && (palavra[j + 1] <= 57)){
+          proximaNum = 1;
+          numero[0] = palavra[j];
+          numero[1] = palavra[j + 1];
+          qnt = atoi(numero);
+          j++;
+        }
+      }
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+      proximaNum = 0;
+      if(palavra[j + 1] >= 48 && palavra[j + 1] <= 57){
+        proximaNum = 1;
+      }
+      if(palavra[j] >= 48 && palavra[j] <= 57){
+        if((j + 1 < strlen(palavra)) && (proximaNum)){
+          
+          j = j + 1;
+          sinal = 1;
+        } else {
+          qnt = atoi(palavra[j]);
+          sinal = 1
+        }
+      } else {
+        switch(palavra[j]){
+          case 'O':
+            peso = OXI;
+            break;
+          case 'C':
+            peso = CARB;
+            break;
+          case 'H':
+            peso = HID;
+            break;
+          case 'N':
+            peso = NIT;
+            break;
+        }
+        if(proximaNum){
+
+        }
+      }
+      if(sinal){
+        mol = mol + qnt*peso;
+        sinal = 0;
       }
     }
     printf("%d\n", score);
